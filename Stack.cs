2deb5b5016f843016f045bc7c2d166bccd03dd98
kennersonpv <kennersondudu@hotmail.com>
2022-07-31 21:08:59 -0300

    First commit

--- Stack.cs
+++ Stack.cs
@@ -1,10 +1,34 @@
-﻿namespace Stack
+﻿using System.Collections.Generic;
+
+namespace Stack
 {
     partial class Program
     {
         public class Stack
         {
+            private readonly List<object> ts;
 
+            public Stack()
+            {
+                ts = new List<object>();
+            }
+        
+            public void Push(object obj)
+            {
+                ts.Add(obj);
+            }
+
+            public object Pop()
+            {
+                var obj = ts[ts.Count - 1];
+                ts.Remove(obj);
+                return obj;
+            }
+
+            public void Clear()
+            {
+                ts.Clear();
+            }
         }
     }
 }
